fe40a2a386c1d435e79ceb9e1c1e79ddbf58310c
Mateusz Kiedra <m.kiedra@student.po.edu.pl>
2024-04-16 15:33:18 +0200

    work in progress

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -5,6 +5,7 @@ const MainPage = () => {
     <div>
       <h1>Main Page</h1>
       <p>Tutaj umieść zawartość strony głównej.</p>
+      <img src="/img/placeholder.jpeg" alt="" />
     </div>
   );
 }
