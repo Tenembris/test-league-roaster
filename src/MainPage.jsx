05ca879cb9fcfcb43f8ee7e043dee377656173ff
Mateusz Kiedra <m.kiedra@student.po.edu.pl>
2024-04-16 15:43:08 +0200

    without component

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -7,7 +7,7 @@ const MainPage = () => {
       <h1>Main Page</h1>
       <p>Tutaj umieść zawartość strony głównej.</p>
       <img src="/img/placeholder.jpeg" alt="" />
-      <Champions />
+      
     </div>
   );
 }
