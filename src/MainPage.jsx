28e1d10eca79bcd3dc67b970cdae909c5a1e2214
Mateusz Kiedra <m.kiedra@student.po.edu.pl>
2024-04-16 20:39:54 +0200

    homepage-wip

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import Champions from './Champions';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
   return (
-    <div>
-      <h1>Main Page</h1>
-      <p>Tutaj umieść zawartość strony głównej.</p>
-      <img src="/img/placeholder.jpeg" alt="" />
-      
-    </div>
+    <>
+      <div className='hero-section'>
+        <video className='video-hero' src="/img/full-hd-league.mp4" autoPlay muted loop />
+        <h1>Welcome to League Roaster</h1>
+        <div className='black-gradient' />
+      </div>
+      <div className='option-section'>
+      <div className='black-gradient-top' />
+      <div className='option-card-container'>
+      <div className='option-card'>
+      <Link to="/champions">Champions</Link>
+        
+      </div>
+      <div className='option-card'>
+      <h4>work in progress</h4>
+      </div>
+
+      <div className='option-card'>
+      <h4>work in progress</h4>
+      </div>
+
+      </div>
+      </div>
+    </>
   );
 }
 
