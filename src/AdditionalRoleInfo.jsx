c2d0eb2964fc972a117a4a93db07980647a1ff16
Mateusz Kiedra <m.kiedra@student.po.edu.pl>
2024-04-22 19:34:48 +0200

    same as previous commit, mistake

--- src/AdditionalRoleInfo.jsx
+++ src/AdditionalRoleInfo.jsx
@@ -16,6 +16,7 @@ const AdditionalRoleInfo = ({ role, onClose }) => {
                 <img className='role-icon-size' src="/img/Tank_icon.webp" alt="" />
                 <h1>Tank</h1>
                 <h5>Frontline Protector</h5>
+                <div className='short-line'></div>
                 <p>As a tank, your goal is to become the shield for your team, absorbing damage and provoking enemies so your allies can deal damage without hindrance. Your fearless presence on the frontline helps maintain stability and enables victory for your team.</p>
                 <h6>Traits</h6>
                 <ul className='ul-role-info'>
@@ -39,6 +40,7 @@ const AdditionalRoleInfo = ({ role, onClose }) => {
                 <img className='role-icon-size' src="/img/Support_icon.webp" alt="" />
                 <h1>Support</h1>
                 <h5>Guardian of Allies</h5>
+                <div className='short-line'></div>
                 <p>Support champions provide crucial assistance to their team by offering protection, healing, crowd control, and utility. They excel at helping their allies survive and thrive during battles.</p>
                 <h6>Traits</h6>
                 <ul className='ul-role-info'>
@@ -61,6 +63,7 @@ const AdditionalRoleInfo = ({ role, onClose }) => {
                 <img className='role-icon-size' src="/img/Mage_icon.png" alt="" />
                 <h1>Mage</h1>
                 <h5>Master of Arcane Arts</h5>
+                <div className='short-line'></div>
                 <p>Mages harness the power of magic to unleash devastating spells upon their enemies. They excel at dealing high burst damage and controlling the battlefield with powerful crowd control abilities.</p>
                 <h6>Traits</h6>
                 <ul className='ul-role-info'>
@@ -83,6 +86,7 @@ const AdditionalRoleInfo = ({ role, onClose }) => {
                 <img className='role-icon-size' src="/img/Fighter_icon.png" alt="" />
                 <h1>Fighter</h1>
                 <h5>Unyielding Warrior</h5>
+                <div className='short-line'></div>
                 <p>Fighters are versatile combatants who excel at both dealing and taking damage. They are durable and possess strong sustained damage output, making them formidable opponents in prolonged engagements.</p>
                 <h6>Traits</h6>
                 <ul className='ul-role-info'>
@@ -105,6 +109,7 @@ const AdditionalRoleInfo = ({ role, onClose }) => {
                 <img className='role-icon-size' src="/img/Marksman_icon.png" alt="" />
                 <h1>Marksman</h1>
                 <h5>Ranged Sharpshooter</h5>
+                <div className='short-line'></div>
                 <p>Marksman champions specialize in dealing consistent ranged damage from a safe distance. They are crucial for taking down objectives and shredding enemy champions during team fights.</p>
                 <h6>Traits</h6>
                 <ul className='ul-role-info'>
