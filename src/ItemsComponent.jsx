36bf709cdb65b73149deed86c5984249e028b5d9
Mateusz Kiedra <m.kiedra@student.po.edu.pl>
2024-04-26 20:53:33 +0200

    ornn items

--- src/ItemsComponent.jsx
+++ src/ItemsComponent.jsx
@@ -17,19 +17,22 @@ const ItemsComponent = () => {
             const item = itemData[key];
             return (
               (item.maps["11"] || item.maps["12"]) && // Przejmujemy się tylko elementami, które mają 11 lub 12, ale...
-              (item.inStore !== false || !item.inStore) // ...i których inStore nie jest false lub undefined
+              (item.inStore === undefined || item.inStore !== false || item.depth !== undefined && item.depth !== false) // ...i których inStore nie jest false lub undefined
+              // ...i które posiadają właściwość depth
             );
           })
           .map(key => ({
-            id: key, // Dodajemy id dla elementu
+            id: key,
             name: itemData[key].name,
             imageUrl: `https://ddragon.leagueoflegends.com/cdn/14.8.1/img/item/${itemData[key].image.full}`,
             description: itemData[key].description,
             gold: itemData[key].gold,
             stats: itemData[key].stats,
-            from: itemData[key].from || [], // Dodajemy elementy 'from' lub pustą tablicę, jeśli brak
+            from: itemData[key].from || [],
             into: itemData[key].into || [],
             baseGold: itemData[key].gold.base,
+            depth: itemData[key].depth || 0,
+            Ornn: itemData[key].requiredAlly // Dodajemy głębokość elementu, jeśli istnieje
           }));
         setItems(processedItems);
 
@@ -45,11 +48,14 @@ const ItemsComponent = () => {
     fetchData();
   }, []);
 
-  const formatDescription = (description) => {
+  
+
+    const formatDescription = (description) => {
     return description.replace(/<font color='#(\w+)'>/g, (match, color) => `<span style="color: #${color}">`)
                       .replace(/<\/font>/g, '</span>')
                       .replace(/<br>/g, '<br />');
   };
+
 
   // Funkcja do obsługi kliknięcia na element
   const handleItemClick = (itemId) => {
@@ -58,7 +64,7 @@ const ItemsComponent = () => {
   };
 
   // Funkcja do filtrowania elementów na podstawie tekstu wyszukiwania
- const filteredItems = items.filter(item =>
+  const filteredItems = items.filter(item =>
     item.name.toLowerCase().includes(searchText) || // Wyszukiwanie po nazwie
     (item.colloq && item.colloq.toLowerCase().includes(searchText)) // Wyszukiwanie po polu colloq
   );
@@ -67,50 +73,98 @@ const ItemsComponent = () => {
     setSearchText(event.target.value);
   };
 
-  // Obiekt z nazwami statystyk
-  const statNames = {
-    FlatCritChanceMod: 'Critical Strike Chance',
-    FlatPhysicalDamageMod: 'Attack Damage',
-    PercentLifeStealMod: 'Life Steal',
-    FlatHPPoolMod: 'Health',
-    PercentAttackSpeedMod: 'Attack Speed',
-    FlatArmorMod: 'Armor',
-    PercentMovementSpeedMod: '% Movement Speed',
+  // Grupowanie przedmiotów w zależności od głębokości
+  const groupedItems = {
+    basic: filteredItems.filter(item => item.depth === 1),
+    epic: filteredItems.filter(item => item.depth === 2),
+    legendary: filteredItems.filter(item => item.depth === 3),
+    starter: filteredItems.filter(item => item.depth === 0),
+    other: filteredItems.filter(item => item.depth > 3),
   };
 
-  // Funkcja do renderowania statystyk
-  const renderStats = () => {
-    return (
-      <ul>
-        {Object.entries(selectedItem.stats).map(([stat, value], index) => {
-          const statName = statNames[stat] || stat;
-          return (
-            <li key={index}>
-              {value} {statName}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  };
+  
 
   return (
     <div className="items-container">
-
       <div className='items-tool-component'>
         <div className="search-box">
           <input type="text" placeholder="Search..." value={searchText} onChange={handleSearchTextChange} />
         </div>
         <div className="items-wrapper">
-          {filteredItems.map((item, index) => (
-            <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
-              <img src={item.imageUrl} alt={item.name} />
-              <span>{item.gold.total}</span>
+        {/* <h4>Basic</h4>
+          <div className="basic-items item-group">
+            {groupedItems.basic.length > 0 && (
+              <>
+                
+                {groupedItems.basic.map((item, index) => (
+                  <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
+                    <img src={item.imageUrl} alt={item.name} />
+                    <span>{item.gold.total}</span>
+                  </div>
+                ))}
+              </>
+            )}
+          </div> */}
+                   <h4>Starter</h4>
+          <div className="starter-items item-group">
+            {groupedItems.starter.length > 0 && (
+              <>
+                
+                {groupedItems.starter.map((item, index) => (
+                  <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
+                    <img src={item.imageUrl} alt={item.name} />
+                    <span>{item.gold.total}</span>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+          <h4>Epic</h4>
+          <div className="epic-items item-group">
+            {groupedItems.epic.length > 0 && (
+              <>
+                
+                {groupedItems.epic.map((item, index) => (
+                  <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
+                    <img src={item.imageUrl} alt={item.name} />
+                    <span>{item.gold.total}</span>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+          
+          <h4>Legendary</h4>
+          <div className="legendary-items item-group">
+            {groupedItems.legendary.length > 0 && (
+              <>
+                
+                {groupedItems.legendary.map((item, index) => (
+                  <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
+                     <img src={item.imageUrl} alt={item.name} style={item.Ornn === "Ornn" ? { filter: 'sepia(100%) saturate(1000%) hue-rotate(30deg)' } : null} />
+                    <span>{item.gold.total}</span>
+                    
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+ 
+          <h4>Other</h4>
+          {groupedItems.other.length > 0 && (
+            <div className="other-items item-group">
+              
+              {groupedItems.other.map((item, index) => (
+                <div key={index} className="item" onClick={() => handleItemClick(item.id)}>
+                  <img src={item.imageUrl} alt={item.name} style={item.Ornn === 'Ornn' ? { filter: 'sepia(100%) saturate(500%) hue-rotate(30deg)' } : null}  />
+                  <span>{item.gold.total}</span>
+                 
+                </div>
+              ))}
             </div>
-          ))}
+          )}
         </div>
       </div>
-
       {selectedItem && (
         <div className='item-description'>
             <h3>Into:</h3>
@@ -127,6 +181,7 @@ const ItemsComponent = () => {
               <div>
                 <h2>{selectedItem.name}</h2>
                 <h4>{selectedItem.gold.total} g</h4>
+                
               </div>
             </div>
 
@@ -143,7 +198,7 @@ const ItemsComponent = () => {
                   const fromItem = items.find(item => item.id === itemId);
                   return (
                     <div key={index} className="related-item" onClick={() => handleItemClick(itemId)}>
-                      <img src={fromItem?.imageUrl} alt={fromItem?.name} />
+                      <img src={fromItem?.imageUrl} alt={fromItem?.name} style={items.requiredAlly === 'Ornn' ? { filter: 'sepia(100%) saturate(1000%) hue-rotate(30deg)' } : null}  />
                     </div>
                   );
                 })}
